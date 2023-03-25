c8566c495aad18e0d61ec2732b9b1da472d8bd91
maloricuevas <maloricrossley@gmail.com>
2023-03-24 23:10:46 -0600

    added a contact form and breaks between divs in lessons/units pages

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function myFunction() {
     }
 }
 
+const form = document.contactForm
+
+document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (form.name.value == "") {
+        alert("You must enter your name before submitting this form");
+    }
+    else if (form.email.value == "") {
+        alert("You must provide your email before submitting this form");
+    }
+    else if (form.textbox.value == "") {
+        alert("You must provide an explanation before submitting this form");
+    }
+    else if (form.contactType.value === "Other") {
+        alert("You must select your response type before submitting this form.");
+    }
+    else {
+        alert("Thank you for your question! We will contact you with a response shortly.");
+    }
+
+})
